eeae91ac013bfdde3e0213b624a4ef25c1cabb57
{ne30MEGANE} <{ne300229@senshu-u.jp}>
2019-10-26 14:54:36 +0900

    難易度変更機能を実装

--- script.js
+++ script.js
@@ -1,15 +1,72 @@
 window.onload = Main;
-document.onclick = Click;
 
-let TextArea
-let message = "";
+let GameArea;
+let CardArea;
+let errorArea;
+let startButton;
+let selectedDiff;
+
+let difficulty;
+let diffNumber;
+
 
 function Main() {
-    TextArea = document.getElementById("textarea");
+    GameArea = document.getElementById("gamearea");
+    CreateTable();
+    GameArea.appendChild(CardArea);
+
+    errorArea = document.getElementById('error');
+    difficulty = document.getElementsByName('diff');
+
+    startButton = document.getElementById('startbutton');
+    startButton.addEventListener("click", buttonAction);
 
 };
 
-function Click(evt) {
-    message = message + "ウオ";
-    TextArea.innerHTML = message;
-};
+function CreateTable() {
+    CardArea = document.createElement('table');
+    for (let i = 0; i < 4; i++) {
+        let tr = document.createElement('tr');
+        for (let j = 0; j < 7; j++) {
+            let td = document.createElement('td');
+            td.textContent = "( " + i + " , " + j + " )"
+            tr.appendChild(td);
+        }
+        CardArea.appendChild(tr);
+    }
+
+}
+
+function buttonAction() {
+    errorArea.innerHTML = "";
+    for (let i = 0; i < difficulty.length; i++) { //難易度を取得
+        if (difficulty[i].checked) {
+            selectedDiff = difficulty[i].value;
+            break;
+        }
+    }
+    if (selectedDiff == undefined) { // 未選択の時
+        errorArea.innerHTML = "難易度を選んでください。";
+    } else { //正しく難易度が選ばれている場合
+        createTarget(selectedDiff); //問題生成
+        console.log(diffNumber);
+    }
+
+}
+
+function createTarget(d) {
+    switch (d) { //まず難易度から問題作成用の数字を決定
+        case "EASY":
+            diffNumber = 4;
+            break;
+        case "NORMAL":
+            diffNumber = 8;
+            break;
+        case "HARD":
+            diffNumber = 12;
+            break;
+        default: //難易度を選ばずにスタートが押されたとき
+            break;
+    }
+    //以下に問題生成の処理を書く
+}
